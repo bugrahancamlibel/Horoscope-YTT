b7d3c845c2359e116a2e97dcf0552e19625dec31
BUGRA <camlibelbugrahan@gmail.com>
2023-02-13 18:07:50 +0300

    Visual improvements in first_choose.dart

--- lib/screens/firts_choose.dart
+++ lib/screens/firts_choose.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-const List<String> list = <String>['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo',
-  'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const List<String> list = <String>[
+  'Aries',
+  'Taurus',
+  'Gemini',
+  'Cancer',
+  'Leo',
+  'Virgo',
+  'Libra',
+  'Scorpio',
+  'Sagittarius',
+  'Capricorn',
+  'Aquarius',
+  'Pisces'
+];
 
 class FirstChoose extends StatefulWidget {
   const FirstChoose({Key? key}) : super(key: key);
@@ -16,27 +28,40 @@ class _FirstChooseState extends State<FirstChoose> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DropdownButton<String>(
-        value: dropdownValue,
-        icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        style: const TextStyle(color: Colors.deepPurple),
-        underline: Container(
-          height: 2,
-          color: Colors.deepPurpleAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text("Select your star sign",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w300,
+                    color: Color(0XFF3F3D56),
+                    height: 2.0)),
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.star_sharp),
+              elevation: 16,
+              style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? value) {
+                // This is called when the user selects an item.
+                setState(() {
+                  dropdownValue = value!;
+                });
+              },
+              items: list.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ],
         ),
-        onChanged: (String? value) {
-          // This is called when the user selects an item.
-          setState(() {
-            dropdownValue = value!;
-          });
-        },
-        items: list.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
       ),
     );
   }
