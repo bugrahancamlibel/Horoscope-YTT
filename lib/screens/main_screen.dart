f3ed0aec37775b22ee71e9920b5d847d6a4bdec2
BUGRA <camlibelbugrahan@gmail.com>
2023-02-14 22:33:20 +0300

    Add nav bar to main page

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:horoscope_ytt/selected_index_provider.dart';
 import '../star_sign_provider.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+
+const TextStyle optionStyle =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+const List<Widget> _widgetOptions = <Widget>[
+  Text(
+    'Yesterday',
+    style: optionStyle,
+  ),
+  Text(
+    'Today',
+    style: optionStyle,
+  ),
+  Text(
+    'Tomorrow',
+    style: optionStyle,
+  ),
+];
 
 class MainScreen extends ConsumerWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -19,15 +39,35 @@ class MainScreen extends ConsumerWidget {
           )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            color: Colors.purple,
-            width: 100,
-            height: 100,
-          )
+      body: _widgetOptions.elementAt(ref.watch(selectedIndexProvider)),
+      bottomNavigationBar: GNav(
+        rippleColor: Colors.grey[300]!,
+        hoverColor: Colors.grey[100]!,
+        gap: 8,
+        activeColor: Colors.purple,
+        iconSize: 24,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        duration: const Duration(milliseconds: 400),
+        tabBackgroundColor: Colors.grey[100]!,
+        color: Colors.black,
+        tabs: const [
+          GButton(
+            icon: Icons.arrow_back_outlined,
+            text: 'Yesterday',
+          ),
+          GButton(
+            icon: Icons.circle,
+            text: 'Today',
+          ),
+          GButton(
+            icon: Icons.arrow_forward_outlined,
+            text: 'Tomorrow',
+          ),
         ],
+        selectedIndex: ref.watch(selectedIndexProvider),
+        onTabChange: (index) {
+          ref.read(selectedIndexProvider.notifier).state = index;
+        },
       ),
     );
   }
