e45d6145d03a49907345dbeb739a562316dd06fc
BUGRA <camlibelbugrahan@gmail.com>
2023-02-19 23:26:54 +0300

    Wrap the content with Stack

--- lib/screens/horoscope_screen.dart
+++ lib/screens/horoscope_screen.dart
@@ -16,162 +16,166 @@ class HoroscopeScreen extends ConsumerWidget {
 
     return SingleChildScrollView(
       child: Center(
-        child: Column(
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Card(
-                color: const Color(0xFFF1E9F7),
-                shadowColor: Colors.purple,
-                elevation: 10,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  height: 140,
-                  child: SingleChildScrollView(
-                    child: ListTile(
-                      title: const Text("Fortune"),
-                      subtitle: Text(
-                          todayHoroscopeConsumer.value?.description.toString() ??
-                              "-"),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Card(
-                color: const Color(0xFFF1E9F7),
-                elevation: 10,
-                shadowColor: Colors.purple,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  child: SingleChildScrollView(
-                    child: ListTile(
-                      title: const Text("Your..."),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                RichText(
-                                  text: TextSpan(
-                                    text: '',
-                                    style: DefaultTextStyle.of(context).style,
-                                    children: <TextSpan>[
-                                      const TextSpan(
-                                          text: 'Lucky Number: ',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                      TextSpan(
-                                          text: todayHoroscopeConsumer
-                                                  .value?.luckyNumber
-                                                  .toString() ??
-                                              "-"),
-                                    ],
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                RichText(
-                                  text: TextSpan(
-                                    text: '',
-                                    style: DefaultTextStyle.of(context).style,
-                                    children: <TextSpan>[
-                                      const TextSpan(
-                                          text: 'Lucky Time: ',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                      TextSpan(
-                                          text: todayHoroscopeConsumer
-                                                  .value?.luckyTime
-                                                  .toString() ??
-                                              "-"),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                RichText(
-                                  text: TextSpan(
-                                    text: '',
-                                    style: DefaultTextStyle.of(context).style,
-                                    children: <TextSpan>[
-                                      const TextSpan(
-                                          text: 'Color: ',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                      TextSpan(
-                                          text: todayHoroscopeConsumer
-                                                  .value?.color
-                                                  .toString() ??
-                                              "-"),
-                                    ],
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                RichText(
-                                  text: TextSpan(
-                                    text: '',
-                                    style: DefaultTextStyle.of(context).style,
-                                    children: <TextSpan>[
-                                      const TextSpan(
-                                          text: 'Mood: ',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                      TextSpan(
-                                          text: todayHoroscopeConsumer.value?.mood
-                                                  .toString() ??
-                                              "-"),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Card(
+                    color: const Color(0xFFF1E9F7),
+                    shadowColor: Colors.purple,
+                    elevation: 10,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      height: 140,
+                      child: SingleChildScrollView(
+                        child: ListTile(
+                          title: const Text("Fortune"),
+                          subtitle: Text(
+                              todayHoroscopeConsumer.value?.description.toString() ??
+                                  "-"),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Card(
-                color: const Color(0xFFF1E9F7),
-                shadowColor: Colors.purple,
-                elevation: 10,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  height: 140,
-                  child: SingleChildScrollView(
-                    child: ListTile(
-                        title: const Text("Compatibility"),
-                        subtitle: Padding(
-                          padding: const EdgeInsets.only(top: 20),
-                          child: Column(
-                            children: [
-                              Image.asset(
-                                "assets/images/${todayHoroscopeConsumer.value?.compatibility.toLowerCase().toString() ?? "1"}.png",
-                                height: 80,
-                              ),
-                              Text(todayHoroscopeConsumer.value?.compatibility
-                                      .toString() ??
-                                  "-"),
-                            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Card(
+                    color: const Color(0xFFF1E9F7),
+                    elevation: 10,
+                    shadowColor: Colors.purple,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      child: SingleChildScrollView(
+                        child: ListTile(
+                          title: const Text("Your..."),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  children: [
+                                    RichText(
+                                      text: TextSpan(
+                                        text: '',
+                                        style: DefaultTextStyle.of(context).style,
+                                        children: <TextSpan>[
+                                          const TextSpan(
+                                              text: 'Lucky Number: ',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                          TextSpan(
+                                              text: todayHoroscopeConsumer
+                                                  .value?.luckyNumber
+                                                  .toString() ??
+                                                  "-"),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    RichText(
+                                      text: TextSpan(
+                                        text: '',
+                                        style: DefaultTextStyle.of(context).style,
+                                        children: <TextSpan>[
+                                          const TextSpan(
+                                              text: 'Lucky Time: ',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                          TextSpan(
+                                              text: todayHoroscopeConsumer
+                                                  .value?.luckyTime
+                                                  .toString() ??
+                                                  "-"),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    RichText(
+                                      text: TextSpan(
+                                        text: '',
+                                        style: DefaultTextStyle.of(context).style,
+                                        children: <TextSpan>[
+                                          const TextSpan(
+                                              text: 'Color: ',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                          TextSpan(
+                                              text: todayHoroscopeConsumer
+                                                  .value?.color
+                                                  .toString() ??
+                                                  "-"),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    RichText(
+                                      text: TextSpan(
+                                        text: '',
+                                        style: DefaultTextStyle.of(context).style,
+                                        children: <TextSpan>[
+                                          const TextSpan(
+                                              text: 'Mood: ',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                          TextSpan(
+                                              text: todayHoroscopeConsumer.value?.mood
+                                                  .toString() ??
+                                                  "-"),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        )),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Card(
+                    color: const Color(0xFFF1E9F7),
+                    shadowColor: Colors.purple,
+                    elevation: 10,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      height: 140,
+                      child: SingleChildScrollView(
+                        child: ListTile(
+                            title: const Text("Compatibility"),
+                            subtitle: Padding(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: Column(
+                                children: [
+                                  Image.asset(
+                                    "assets/images/${todayHoroscopeConsumer.value?.compatibility.toLowerCase().toString() ?? "1"}.png",
+                                    height: 80,
+                                  ),
+                                  Text(todayHoroscopeConsumer.value?.compatibility
+                                      .toString() ??
+                                      "-"),
+                                ],
+                              ),
+                            )),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
